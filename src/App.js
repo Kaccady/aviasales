c2861cc60dd8885095de64fc79024be0719a1273
Kaccady <z-rz@mail.com>
2019-03-28 18:58:18 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+const Checkbox = props => {
+  return (
+    <div className="checkbox">
+      <input type="checkbox" />
+      {props.name}
+    </div>
+  );
+};
+
+const elems = [
+  "Без пересадок",
+  "1 Пересадка",
+  "2 Пересадки",
+  "3 Пересадки"
+].map(function(props) {
+  return <Checkbox name={props} />;
+});
+
+class Ticket extends Component {
+  render() {
+    return <div className='ticket'> <div className='column'><a href='/'className='t-logo'/>
+    <button className='buy-button'>купить за 21 032 р</button></div>
+    </div>
+    ;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <a href="/" className="logo" />
+        <div className="content">
+          <div className="filters">
+            <p>ВАЛЮТА</p>
+            <div className="buttons">
+              <button>RUB</button>
+              <button>USD</button>
+              <button>EUR</button>
+            </div>
+            <p>КОЛИЧЕСТВО ПЕРЕСАДОК</p>
+            <Checkbox name="Все" />
+            {elems}
+          </div>
+          <div className="tickets"><Ticket/></div>
+        </div>
       </div>
     );
   }
